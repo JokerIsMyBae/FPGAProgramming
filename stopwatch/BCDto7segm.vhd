e26027859e62ce706d93f3030df056365c7f9666
Joos Van Esbroeck <58085617+JokerIsMyBae@users.noreply.github.com>
2022-05-24 09:34:01 +0200

    Added comments

--- stopwatch/BCDto7segm.vhd
+++ stopwatch/BCDto7segm.vhd
@@ -9,8 +9,8 @@ end BCDto7segm;
 
 architecture Behavioral of BCDto7segm is
 
-type tSegm is array(0 to 9) of std_logic_vector(6 downto 0);
-    constant cSegm : tSegm := ("1000000", -- 0
+type tSegm is array(0 to 9) of std_logic_vector(6 downto 0); --nieuwe type die array van std_logic_vectors bevat
+    constant cSegm : tSegm := ("1000000", -- 0 --std_logic_vectors zijn de cijfers van 0 tot 9
                                "1111001", -- 1
                                "0100100", -- 2
                                "0110000", -- 3
@@ -23,10 +23,10 @@ type tSegm is array(0 to 9) of std_logic_vector(6 downto 0);
 
 begin
 
-process (x)
+process (x) --input in sensititvity list
 begin
     if unsigned(x) < 10 then      		
-        g_to_a <= cSegm(to_integer(unsigned(x)));
+        g_to_a <= cSegm(to_integer(unsigned(x))); --zet x (std_logic_vector) om naar int, om als index van cSegm te gebruiken
     else -- geef 'e' weer om te tonen dat er een error is, namelijk een getal groter dan 9
         g_to_a <= "0000110";
     end if;
